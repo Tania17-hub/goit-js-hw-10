308df1bcf1b2ab2a13b240e1c3ed523d66959d86
Тетяна <yabanshi.tania@gmail.com>
2025-06-02 22:40:44 +0300

    update index html file

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -13,4 +13,45 @@ function showErrorMessage(message) {
   });
 }
 
-fu;
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = parseInt(delayInput.value);
+  const selectedState = [...stateInputs].find(input => input.checked)?.value;
+
+  if (!delay || isNaN(delay)) {
+    showErrorMessage('Please enter a valid delay.');
+    return;
+  }
+
+  if (!selectedState) {
+    showErrorMessage('Please select promise state (fulfilled or rejected).');
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (selectedState === 'fulfilled') {
+        resolve(`✅ Fulfilled in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected in ${delay}ms`);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(message => {
+      iziToast.success({
+        title: 'Success',
+        message: message,
+        position: 'topRight',
+      });
+    })
+    .catch(message => {
+      iziToast.error({
+        title: 'Error',
+        message: message,
+        position: 'topRight',
+      });
+    });
+});
